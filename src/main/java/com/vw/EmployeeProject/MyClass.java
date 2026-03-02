ed2bf16ee15e7b3fce29bc190e8f9d1e952e4c17
Utkarsh Pandey <utk2665@gmail.com>
2026-03-02 16:58:06 +0530

    Init

---
+++ src/main/java/com/vw/EmployeeProject/MyClass.java
@@ -0,0 +1,16 @@
+package com.vw.EmployeeProject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+
+
+    @GetMapping("abc")
+    public String sayHello(){
+        return "Hello";
+    }
+
+}
